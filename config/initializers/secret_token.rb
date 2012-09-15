d281ed43ed2c4ca27157708642573849b4d885af
Kvanh Varma <kvanhvarma@me.com>
2012-09-15 12:58:03 +0530

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kushweb::Application.config.secret_token = '30b0076f33e3f485697f87f2df7e7a198124439aef979387048880beab81f5a1a90103a70b45f85bf13c595e5987474aedc06465999237cde4c280c0b97c37f0'
